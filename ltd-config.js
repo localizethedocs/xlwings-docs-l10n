5ddeeef1d2a41a0d6d9b49d5b7811ac477050793
ltdrobot <ltdrobot@localizethedocs.org>
2026-08-31 17:24:06 +0000

    Deploying to pages from @ localizethedocs/xlwings-docs-l10n@264dfa0d05f736de7712b2d0423b6ab3943eb6d5 🚀

---
+++ ltd-config.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["latest",  "Development"],
+    ["stable",  "Stable Release"],
+    ["0",       "Release 0.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/xlwings-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/xlwings-docs-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/xlwings-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/xlwings-docs-l10n"],
+  ]
+};
